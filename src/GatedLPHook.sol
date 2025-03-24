ad92e1484f1dfa1e34b81992493f3ff2f47b27b4
Bolaji Ahmad <56865496+bolajahmad@users.noreply.github.com>
2025-03-24 12:12:07 +0100

    setup hooks base permissions function; initialized constructor

--- src/GatedLPHook.sol
+++ src/GatedLPHook.sol
@@ -6,6 +6,34 @@ pragma solidity ^0.8.13;
 ** Should contain the beforeLiquidity hooks (add & remove)
 */
 
-contract GatedLPHook {
-    
+import { BaseHook } from "v4-periphery/src/utils/BaseHook.sol";
+
+import { PoolId, PoolIdLibrary } from "@uniswap/v4-core/src/types/PoolId.sol";
+import { PoolKey } from "v4-core/src/types/PoolKey.sol";
+import { Hooks } from "v4-core/src/libraries/Hooks.sol";
+import { IPoolManager } from "v4-core/src/interfaces/IPoolManager.sol";
+
+contract GatedLPHook is BaseHook {
+    using PoolIdLibrary for PoolKey;
+
+    constructor(IPoolManager _poolManager) BaseHook(_poolManager) {}
+
+    function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
+        return Hooks.Permissions({
+            beforeInitialize: false,
+            afterInitialize: false,
+            beforeAddLiquidity: true,
+            afterAddLiquidity: false,
+            beforeRemoveLiquidity: true,
+            afterRemoveLiquidity: false,
+            beforeSwap: true,
+            afterSwap: true,
+            beforeDonate: false,
+            afterDonate: false,
+            beforeSwapReturnDelta: false,
+            afterSwapReturnDelta: false,
+            afterAddLiquidityReturnDelta: false,
+            afterRemoveLiquidityReturnDelta: false
+        });
+    }
 }
