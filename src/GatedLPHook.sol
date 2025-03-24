b988c5be8ad7d37d2fb82608fdc16de7086fc362
Bolaji Ahmad <56865496+bolajahmad@users.noreply.github.com>
2025-03-24 10:45:50 +0100

    initalizing new uniswap hook ligic

---
+++ src/GatedLPHook.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+/*
+** Uniswap hook implementation for our GatedLP contract
+** Should contain the beforeLiquidity hooks (add & remove)
+*/
+
+contract GatedLPHook {
+    
+}
