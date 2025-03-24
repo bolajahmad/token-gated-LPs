de31a0246beb58f70271b11a49666716a6dd4721
Bolaji Ahmad <56865496+bolajahmad@users.noreply.github.com>
2025-03-24 14:20:37 +0100

    added extra logic to initialize the structure of the liquidityAdd hook

--- src/GatedLPHook.sol
+++ src/GatedLPHook.sol
@@ -16,18 +16,21 @@ import { IPoolManager } from "v4-core/src/interfaces/IPoolManager.sol";
 contract GatedLPHook is BaseHook {
     using PoolIdLibrary for PoolKey;
 
+    // Only one LP is expected to connect to this hook anyway
+    uint256 private beforeAddLiquidityCount;
+
     constructor(IPoolManager _poolManager) BaseHook(_poolManager) {}
 
     function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
         return Hooks.Permissions({
+            beforeAddLiquidity: true,
+            beforeRemoveLiquidity: true,
             beforeInitialize: false,
             afterInitialize: false,
-            beforeAddLiquidity: true,
             afterAddLiquidity: false,
-            beforeRemoveLiquidity: true,
             afterRemoveLiquidity: false,
-            beforeSwap: true,
-            afterSwap: true,
+            beforeSwap: false,
+            afterSwap: false,
             beforeDonate: false,
             afterDonate: false,
             beforeSwapReturnDelta: false,
@@ -35,5 +38,22 @@ contract GatedLPHook is BaseHook {
             afterAddLiquidityReturnDelta: false,
             afterRemoveLiquidityReturnDelta: false
         });
+    }
+
+    function _beforeAddLiquidity(
+        address sender,
+        PoolKey calldata key,
+        IPoolManager.ModifyLiquidityParams calldata params,
+        bytes calldata hookdata
+    ) internal override returns (bytes4) {
+        // TODO: Check that the key.id() is for our LP
+        beforeAddLiquidityCount++;
+        // Check if user has NFT
+        return BaseHook.beforeAddLiquidity.selector;
+    }
+
+    // TODO: Update `address token` to use type of NFT contract
+    function updateNFTContract(address token) external {
+        // update contract address
     }
 }
